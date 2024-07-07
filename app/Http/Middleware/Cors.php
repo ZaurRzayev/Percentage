fdd829cc3a1697f2a65909efd958452784d983e1
Zaur Rzayev <61623422+ZaurRzayev@users.noreply.github.com>
2024-07-07 18:58:26 +0400

    commit the testing envoirment

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -21,7 +21,7 @@ class Cors
             ->header('Access-Control-Allow-Credentials', true)
             ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
             ->header('Accept', 'application/json')
-            ->header('path', 'api/*')
-            ->header('path', 'dashboard/');
+            ->header('path', 'api/*');
+
     }
 }
